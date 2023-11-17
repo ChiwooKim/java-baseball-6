44781cb91d4ff783e3b7a6a2c7b1b2a884842294
kcw0360 <kcw0360@naver.com>
2023-11-17 23:33:05 +0900

    feat(OutputView): 숫자 입력 메시지 메소드 분리

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -4,7 +4,9 @@ public class OutputView {
 
     public static void printStart() {
         System.out.println("숫자 야구 게임을 시작합니다.");
-        System.out.println("숫자를 입력해주세요 : ");
     }
 
+    public static void printInputNumbers() {
+        System.out.println("숫자를 입력해주세요 : ");
+    }
 }
