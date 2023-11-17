f3800b5db32aca5d1ed24640edd7d9b6150b5141
kcw0360 <kcw0360@naver.com>
2023-11-17 21:02:32 +0900

    feat(Validator): 중복 입력값 검증 메서드 작성

--- src/main/java/baseball/util/Validator.java
+++ src/main/java/baseball/util/Validator.java
@@ -1,13 +1,17 @@
 package baseball.util;
 
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class Validator {
 
     private static final Pattern REGEX_NUMBERS = Pattern.compile("^[1-9]{3}$");
+    private static final int NUMBERS_LENGTH = 3;
 
-    public static void validate(String input) {
-        validateNumbersPattern(input);
+    public static void validateDuplication(List<Integer> input) {
+        if (input.size() < NUMBERS_LENGTH) {
+            throw new IllegalArgumentException("중복된 숫자가 존재합니다. 다시 입력해주세요.");
+        }
     }
 
     private static void validateNumbersPattern(String input) {
