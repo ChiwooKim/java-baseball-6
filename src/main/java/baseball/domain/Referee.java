3f1276be0bfb97787023755d1b2d3cf41b293f1c
kcw0360 <kcw0360@naver.com>
2023-11-18 00:06:13 +0900

    feat(Referee): 볼, 스트라이크 카운트 하는 메서드 분리

--- src/main/java/baseball/domain/Referee.java
+++ src/main/java/baseball/domain/Referee.java
@@ -7,21 +7,25 @@ public class Referee {
     private int ball;
     private int strike;
 
-    public Referee() {
-        this.ball = 0;
-        this.strike = 0;
+    public void checkResult(List<Integer> userNumbers, List<Integer> computerNumbers) {
+        List<Integer> result = countBallAndStrike(userNumbers, computerNumbers);
+        this.ball = result.get(0);
+        this.strike = result.get(1);
     }
 
-    public void checkResult(List<Integer> userNumbers, List<Integer> computerNumbers) {
+    private List<Integer> countBallAndStrike(List<Integer> userNumbers, List<Integer> computerNumbers) {
+        int ball = 0;
+        int strike = 0;
+        
         for (int i = 0; i < userNumbers.size(); i++) {
             if (userNumbers.get(i).equals(computerNumbers.get(i))) {
                 strike++;
                 continue;
             }
-
             if (computerNumbers.contains(userNumbers.get(i))) {
                 ball++;
             }
         }
+        return List.of(ball, strike);
     }
 }
