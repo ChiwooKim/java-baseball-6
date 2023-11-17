5bb52accd686d061b47dca12de077ac948f69fb5
kcw0360 <kcw0360@naver.com>
2023-11-18 00:05:07 +0900

    feat(GameController): 종료 조건 만족할 때까지 반복하는 로직 작성

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,13 +1,39 @@
 package baseball.controller;
 
+import baseball.domain.Computer;
+import baseball.domain.Referee;
+import baseball.util.GameProgress;
 import baseball.view.InputView;
 import baseball.view.OutputView;
 import java.util.List;
 
 public class GameController {
 
+    private final Computer computer;
+    private final Referee referee;
+    private GameProgress gameProgress;
+
+    public GameController() {
+        this.computer = new Computer();
+        this.referee = new Referee();
+        this.gameProgress = GameProgress.START;
+    }
+
     public void play() {
+        playOrEnd();
+    }
+
+    private void playOrEnd() {
         OutputView.printStart();
-        List<Integer> numbers = InputView.readNumbers();
+        while (!GameProgress.isQuit(gameProgress)) {
+            OutputView.printInputNumbers();
+            List<Integer> computerNumbers = computer.generateRandomNumbers();
+            playUntilThreeStrikes(computerNumbers);
+        }
+    }
+
+    private void playUntilThreeStrikes(List<Integer> computerNumbers) {
+        List<Integer> userNumbers = InputView.readNumbers();
+        referee.checkResult(userNumbers, computerNumbers);
     }
 }
