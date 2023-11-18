c940f48278d174d05cb39a80c0eaf2ec42ffc546
kcw0360 <kcw0360@naver.com>
2023-11-19 03:48:00 +0900

    feat(GameProgress): 불필요한 메소드 제거

--- src/main/java/baseball/util/GameProgress.java
+++ src/main/java/baseball/util/GameProgress.java
@@ -4,10 +4,6 @@ public enum GameProgress {
 
     START, RETRY, QUIT;
 
-    public static boolean isRetry(GameProgress gameProgress) {
-        return gameProgress == RETRY;
-    }
-
     public static boolean isQuit(GameProgress gameProgress) {
         return gameProgress == QUIT;
     }
