fd072785b7405f09a818f2773fa84a11aaaa628b
kcw0360 <kcw0360@naver.com>
2023-11-19 02:10:40 +0900

    feat(GameProgress): 재시작 여부 체크하는 메소드 작성

--- src/main/java/baseball/util/GameProgress.java
+++ src/main/java/baseball/util/GameProgress.java
@@ -4,6 +4,10 @@ public enum GameProgress {
 
     START, RETRY, QUIT;
 
+    public static boolean isRetry(GameProgress gameProgress) {
+        return gameProgress == RETRY;
+    }
+
     public static boolean isQuit(GameProgress gameProgress) {
         return gameProgress == QUIT;
     }
