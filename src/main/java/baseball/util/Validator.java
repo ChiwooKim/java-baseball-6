cdd8a3bc42f08ff8863514ddf175c72c4a84d4e4
kcw0360 <kcw0360@naver.com>
2023-11-19 03:04:41 +0900

    feat(Validator): 재시작 or 종료 입력 검증 메소드 작성

--- src/main/java/baseball/util/Validator.java
+++ src/main/java/baseball/util/Validator.java
@@ -6,6 +6,7 @@ import java.util.regex.Pattern;
 public class Validator {
 
     private static final Pattern REGEX_NUMBERS = Pattern.compile("^[1-9]{3}$");
+    private static final Pattern REGEX_NUMBER = Pattern.compile("^[1-2]{1}$");
     private static final int NUMBERS_LENGTH = 3;
 
     public static void validateDuplication(List<Integer> input) {
@@ -17,6 +18,12 @@ public class Validator {
     public static void validateNumbersPattern(String input) {
         if (!REGEX_NUMBERS.matcher(input).matches()) {
             throw new IllegalArgumentException("1부터 9까지의 숫자 중 3가지 숫자를 입력해주세요.");
+        }
+    }
+
+    public static void validateNumberPattern(String input) {
+        if (!REGEX_NUMBER.matcher(input).matches()) {
+            throw new IllegalArgumentException("재시작을 원하면 1, 종료를 원하면 2를 입력해주세요.");
         }
     }
 }
