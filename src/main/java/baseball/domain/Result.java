5bb9490a59656fa70b7b753b6d58907760bc7879
kcw0360 <kcw0360@naver.com>
2023-11-19 03:47:26 +0900

    feat(GameProgress): 불필요한 메소드 제거

--- src/main/java/baseball/domain/Result.java
+++ src/main/java/baseball/domain/Result.java
@@ -1,11 +1,11 @@
 package baseball.domain;
 
-import baseball.util.ResultMessage;
 import java.util.List;
 
 public class Result {
 
     private static final int DEFAULT_NUMBER = 0;
+    private static final int END_NUMBER = 3;
 
     private int ball;
     private int strike;
@@ -29,11 +29,37 @@ public class Result {
         }
     }
 
-    public boolean hasBall() {
-        return ball > DEFAULT_NUMBER;
+    public boolean isThreeStrikes() {
+        return strike == END_NUMBER;
     }
 
     public String announceResult() {
-        return ResultMessage.getResultMessage(ball, strike);
+        if (ball == 0 && strike == 0) {
+            return Message.NOTHING.getMessage();
+        }
+        if (ball == 0) {
+            return String.format(Message.STRIKE.getMessage(), strike);
+        }
+        if (strike == 0) {
+            return String.format(Message.BALL.getMessage(), ball);
+        }
+        return String.format(Message.BALL_AND_STRIKE.getMessage(), ball, strike);
+    }
+
+    private enum Message {
+        NOTHING("낫싱"),
+        BALL("%d볼"),
+        STRIKE("%d스트라이크"),
+        BALL_AND_STRIKE("%d볼 %d스트라이크");
+
+        private final String message;
+
+        Message(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
     }
 }
