1624596edb186d44a1c85f15911e7178869a5942
kcw0360 <kcw0360@naver.com>
2023-11-19 16:49:52 +0900

    chore(GameProgress): 매직넘버 수정

--- src/main/java/baseball/util/GameProgress.java
+++ src/main/java/baseball/util/GameProgress.java
@@ -4,12 +4,14 @@ public enum GameProgress {
 
     START, RETRY, QUIT;
 
+    private static final int RETRY_NUMBER = 1;
+
     public static boolean isQuit(GameProgress gameProgress) {
         return gameProgress == QUIT;
     }
 
     public static GameProgress judgeEnd(int number) {
-        if (number == 1) {
+        if (number == RETRY_NUMBER) {
             return START;
         }
         return QUIT;
